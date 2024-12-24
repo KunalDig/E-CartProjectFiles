b72e1a16051cf6599af3ba5c29acc29f7511c879
kunal <kunaldigdeotulwar@gmail.com>
2024-12-24 23:58:56 +0530

    functions.txt which tells briefly about admin and normal user functionality...

--- SourceCode/EcartApp/src/main/java/client/EcartApplication.java
+++ SourceCode/EcartApp/src/main/java/client/EcartApplication.java
@@ -5,7 +5,7 @@ import repository.DBConfig;
 public class EcartApplication {
 
 	public static void main(String[] args) {
-		DBConfig config = DBConfig.getInstance();
+//		DBConfig config = DBConfig.getInstance();
 
 	}
 
